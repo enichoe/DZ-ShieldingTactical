8fef49081ad9f0727e685e3dbc62cc49c9c0ac22
Ernesto Nicho <enichoe@gmail.com>
2026-03-07 13:35:48 -0500

    Subiendo versión con QR de billteras

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -4,7 +4,7 @@
 
 const SUPABASE_URL = 'https://igcrevixkxqguawipaon.supabase.co';
 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlnY3Jldml4a3hxZ3Vhd2lwYW9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4OThmMjMsImV4cCI6MjA4ODQyNTgyM30.9AsD7X-n4LU4H37rwigUlBdSef8XUj7-oVh2POr-sNU';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlnY3Jldml4a3hxZ3Vhd2lwYW9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4NDk4MjMsImV4cCI6MjA4ODQyNTgyM30.9AsD7X-n4LU4H37rwigUlBdSef8XUj7-oVh2POr-sNU';
 
 // Crear cliente
 const client = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
